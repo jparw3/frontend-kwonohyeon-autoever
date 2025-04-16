872a367d13ccde989639628e599a49c532e5db61
oyeon-kwon <rnjsdhdus93@gmail.com>
2025-04-16 16:26:33 +0900

    fix: svg 수정

--- src/assets/icons/ClearIcon.tsx
+++ src/assets/icons/ClearIcon.tsx
@@ -9,17 +9,17 @@ interface Props extends ComponentProps<typeof Svg> {
 export default function ClearIcon({ color = "#CDD0D2", ...props }: Props) {
   return (
     <Svg width={20} height={20} viewBox="0 0 20 20" fill="none" {...props}>
-      <g clip-path="url(#clip0_1234_11089)">
+      <g clipPath="url(#clip0_1234_11089)">
         <rect width="20" height="20" rx="10" fill="#F8F8F8" />
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M13.5775 14.756L5.24414 6.42265L6.42265 5.24414L14.756 13.5775L13.5775 14.756Z"
           fill={color}
         />
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M5.24426 13.5775L13.5776 5.24414L14.7561 6.42265L6.42277 14.756L5.24426 13.5775Z"
           fill={color}
         />
